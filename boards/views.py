2df08840694fee8a38a175fa988c4179c67bc554
kuihtalam <talamkuih@gmail.com>
2017-10-18 14:44:56 +0000

    Add text

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+# boards/views.py
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('ABC Realty')
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
